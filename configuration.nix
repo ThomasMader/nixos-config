241ffd3dcf1f5eb26b5caf5bbfc8dbc58463e69a
Thomas Mader <thomas.mader@gmail.com>
2015-11-22 19:38:47 +0100

    Adding initial NixOS configuration

---
+++ configuration.nix
@@ -0,0 +1,89 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda";
+
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "de";
+    defaultLocale = "de_AT.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Vienna";
+
+  nixpkgs.config = {
+
+    allowUnfree = true;
+
+    chromium = {
+      enablePepperFlash = true;
+      enablePepperPDF = true;
+    };
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    nix-repl git vimHugeX chromium
+  ];
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    layout = "at";
+    synaptics.enable = true;
+    synaptics.twoFingerScroll = true;
+
+    # Doesn't work yet in my virtual box image.
+    # displayManager.gdm.enable = true;
+    desktopManager.gnome3.enable = true;
+
+    displayManager.auto = {
+      enable = true;
+      user = "thomad";
+    };
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.thomad = {
+    isNormalUser = true;
+    uid = 1000;
+    createHome = true;
+    home = "/home/thomad";
+    password = "thomad";
+    useDefaultShell = true;
+    group = "users";
+    extraGroups = [ "wheel" "networkmanager" "vboxsf" ];
+  };
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "15.09";
+
+}
