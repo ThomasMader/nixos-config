57a4520db480c998952ed6281c3311f4b9c0899f
Thomas Mader <thomas.mader@gmail.com>
2018-05-12 10:20:44 +0200

    Switch auto login settings to gdm

--- mini-configuration.nix
+++ mini-configuration.nix
@@ -14,7 +14,7 @@
 
   networking.hostName = "mini";
 
-  services.xserver.displayManager.auto = {
+  services.xserver.displayManager.gdm.autoLogin = {
     enable = true;
     user = "thomad";
   };
