83e9b5797b4c2f4bea38c2f4d20c8494f8255ea5
Thomas Mader <thomas.mader@gmail.com>
2023-10-20 21:48:10 +0200

    Add locale config

--- core.nix
+++ core.nix
@@ -41,7 +41,12 @@
 
   # Select internationalisation properties.
   i18n = {
+    supportedLocales = [ "en_US.UTF-8/UTF-8" ];
     defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LANGUAGE = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+    };
   };
 
   console.font = "Lat2-Terminus16";
