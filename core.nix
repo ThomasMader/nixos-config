18811973f71e0fb7cbfbc665b3be962192892054
Thomas Mader <thomas.mader@gmail.com>
2023-11-19 11:26:43 +0100

    Add clang for compiling treesitter plugins

--- core.nix
+++ core.nix
@@ -77,7 +77,7 @@
 
   environment.systemPackages =
     with pkgs; [
-      binutils vim git gh google-chrome pciutils usbutils gparted vlc efibootmgr kdiff3 curl audacious exfat ntfs3g hdparm mplayer gptfdisk glxinfo wol rdesktop wget ripgrep fzf signal-desktop gimp tmux xorg.xkbcomp
+      binutils vim git gh google-chrome pciutils usbutils gparted vlc efibootmgr kdiff3 curl audacious exfat ntfs3g hdparm mplayer gptfdisk glxinfo wol rdesktop wget ripgrep fzf signal-desktop gimp tmux xorg.xkbcomp clang
   ];
 
   programs.neovim = {
