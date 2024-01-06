a0bb031b7a325c245c676f8336a6aec7cfc5d24d
Thomas Mader <thomas.mader@gmail.com>
2024-01-06 13:12:22 +0100

    Add NixOS-WSL as flake input to fix the flake for WSL

--- flake.nix
+++ flake.nix
@@ -2,12 +2,20 @@
   description = "A very basic flake";
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+  inputs.NixOS-WSL = {
+    url = "github:nix-community/NixOS-WSL";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, NixOS-WSL }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ ./wsl-configuration.nix ];
+        modules = [ 
+          { nix.registry.nixpkgs.flake = nixpkgs; }
+          ./wsl-configuration.nix 
+          NixOS-WSL.nixosModules.wsl
+        ];
       };
       nine = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
--- wsl-configuration.nix
+++ wsl-configuration.nix
@@ -9,8 +9,6 @@
 
 {
   imports = [
-    # include NixOS-WSL modules
-    <nixos-wsl/modules>
     ./core.nix
   ];
 
