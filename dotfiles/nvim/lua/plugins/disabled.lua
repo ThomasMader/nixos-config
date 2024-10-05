48e2fd91febc00b4de3d6bb953b2ae76f394ec67
Thomas Mader <thomas.mader@gmail.com>
2024-10-05 13:15:23 +0200

    Completely disable neo-tree for now to fix hijack_netrw_behavior problems

--- dotfiles/nvim/lua/plugins/disabled.lua
+++ dotfiles/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,4 @@
 return {
   -- Do not disable for now because it seems useful for the buffer explorer (<leader>be) at least.
-  -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
--- dotfiles/nvim/lua/plugins/extend-neo-tree.lua
+++ dotfiles/nvim/lua/plugins/extend-neo-tree.lua
@@ -1,9 +1,9 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      -- Needed for making mini.files the default.
-      hijack_netrw_behavior = false,
-    },
-  },
+  -- "nvim-neo-tree/neo-tree.nvim",
+  -- opts = {
+  --   filesystem = {
+  --     -- Needed for making mini.files the default.
+  --     hijack_netrw_behavior = false,
+  --   },
+  -- },
 }
